9bbaf3d12a59ce35ab8d033792f35a221c0527d0
jevinjojo <jevinjojo1@gmail.com>
2026-01-03 21:53:36 +0530

    contract ABI to frontend for deployment

--- frontend/src/lib/contract.ts
+++ frontend/src/lib/contract.ts
@@ -1,4 +1,4 @@
-import CertificateNFT from "../../../contracts/artifacts/contracts/CertificateNFT.sol/CertificateNFT.json";
+import CertificateNFT from "../contracts/CertificateNFT.json";
 
 export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CERTIFICATE_NFT_ADDRESS as `0x${string}`;
 export const CONTRACT_ABI = CertificateNFT.abi; 
